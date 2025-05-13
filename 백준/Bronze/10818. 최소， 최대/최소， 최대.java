5ef4201950ee1e8c0b7d97eef0a9ccfe117cf925
강민기 <131340181+KangMinGi92@users.noreply.github.com>
2025-05-13 18:19:35 +0900

    [Bronze III] Title: 최소, 최대, Time: 524 ms, Memory: 136416 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            int a=Integer.parseInt(br.readLine());
+            String[] arr = br.readLine().split(" ");
+            List<Integer> list =new ArrayList<>();
+            for(int i=0;i<a;i++){
+                list.add(Integer.valueOf(arr[i]));
+            }
+            int max= Collections.max(list);
+            int min= Collections.min(list);
+            bw.write(min+" "+max);
+            bw.flush();
+            bw.close();
+        }
+    }// main
+}// class
