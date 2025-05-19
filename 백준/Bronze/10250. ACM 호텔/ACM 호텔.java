671c9960cffebe910271efd3aaeeaf29bbbaf812
강민기 <131340181+KangMinGi92@users.noreply.github.com>
2025-05-19 17:25:38 +0900

    [Bronze III] Title: ACM 호텔, Time: 208 ms, Memory: 18164 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int a = 0;
+        int b = 0;
+        for (int i = 0; i < t; i++) {
+            int h = sc.nextInt();
+            int w = sc.nextInt();
+            int n = sc.nextInt();
+            if (n % h != 0) {
+                a = n % h * 100;
+                b = n / h + 1;
+            } else {
+                a = h * 100;
+                b = n / h;
+            }
+            System.out.println(a + b);
+        }
+    }// main
+}// class
